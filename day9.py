032133e98034f65d2959c21339315e3ba3c76e31
Dhanush Vaka <dhanush.vaka01@gmail.com>
2026-06-21 08:15:40 +0530

    Update day9.py
    git add .
    git commit -m "Day 9 - Lists completed"
    git push

--- day9.py
+++ day9.py
@@ -1,1 +1,41 @@
- 
+Day 9 – Lists
+
+#Challenge 1: Create a List
+
+fruits = ["Apple", "Banana", "Mango"]
+
+print(fruits)
+
+
+#Challenge 2: Access Elements
+
+fruits = ["Apple", "Banana", "Mango"]
+
+print(fruits[0])
+print(fruits[1])
+
+
+#Challenge 3: Add Item
+
+fruits = ["Apple", "Banana"]
+
+fruits.append("Mango")
+
+print(fruits)
+
+
+#Challenge 4: Remove Item
+
+fruits = ["Apple", "Banana", "Mango"]
+
+fruits.remove("Banana")
+
+print(fruits)
+
+
+#Challenge 5: Loop Through List
+
+fruits = ["Apple", "Banana", "Mango"]
+
+for fruit in fruits:
+    print(fruit) 
