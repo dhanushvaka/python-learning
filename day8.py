769c9bd66e00e0c6d60a17e60f318992ea108c69
Dhanush Vaka <dhanush.vaka01@gmail.com>
2026-06-21 08:13:14 +0530

    Update day8.py
    Day 8
    Topics:
    Functions
    Parameters
    Return Values

--- day8.py
+++ day8.py
@@ -1,1 +1,29 @@
- 
+Day 8
+Topics:
+Functions
+Parameters
+Return Values
+
+#Challenge 1:
+
+def greet(name):
+    print(f"Hello {name}")
+
+greet("Dhanu")
+
+
+#Challenge 2:
+
+
+def square(num):
+    return num * num
+
+print(square(5))
+
+#Challenge 3:
+
+
+def add(a, b):
+    return a + b
+
+print(add(10, 20)) 
